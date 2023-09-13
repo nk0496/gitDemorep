67434feba4e8e63dd53a2cf358735c9ecb0b6dfb
vishalkonale <144889061+vishalkonale@users.noreply.github.com>
2023-09-13 14:46:34 +0530

    nnnn

---
+++ src/gitpackage/Myclass.java
@@ -0,0 +1,21 @@
+package gitpackage;
+
+import java.util.ArrayList;
+
+public class Myclass {
+	
+		public static void main(String[] args) {
+			ArrayList <String> al =new ArrayList<>();
+			al.add("Ravi");
+			al.add("pratik");
+			al.add("me");
+			al.add("sumit");
+			
+			System.out.println(al);
+
+		}
+
+	}
+
+
+
