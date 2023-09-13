ac812203b31d933393d810e2a1195ed7224673e5
nikse <nikse@192.168.1.65>
2023-09-13 15:29:42 +0530

    success

--- src/gitpackage/Gitdemo1.java
+++ src/gitpackage/Gitdemo1.java
@@ -4,6 +4,15 @@ public class Gitdemo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String s = "nikita", nstr = "";
+		char ch;
+		for (int i = 0; i <= s.length(); i++) {
+
+			ch = s.charAt(i);
+			nstr = ch + nstr;
+
+		}
+		System.out.println(nstr);
 
 	}
 
