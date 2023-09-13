c86e60f27ceaa3924192f5105425c4026c4ba5e1
Ketan <kpharate@gmail.com>
2023-09-13 14:54:26 +0530

    Demo

---
+++ src/gitpackage/Calculator.java
@@ -0,0 +1,41 @@
+package gitpackage;
+import java.util.*;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+	       int a = sc.nextInt();
+	       int b = sc.nextInt();
+	       int operator = sc.nextInt();
+	       
+	       switch(operator) {
+           case 1 : System.out.println(a+b);
+           break;
+           case 2 : System.out.println(a-b);
+           break;
+           case 3 : System.out.println(a*b);
+           break;
+           case 4 : if(b == 0) {
+                       System.out.println("Invalid Division");
+                   } else {
+                       System.out.println(a/b);
+                   }
+	    break;
+           case 5 : if(b == 0) {
+                       System.out.println("Invalid Division");
+                   } else {
+                       System.out.println(a%b);
+                   }
+	    break; 
+           default : System.out.println("Invalid Operator");
+       }
+   }
+
+
+	}
+
+
+
+
---
+++ src/gitpackage/Loop.java
@@ -0,0 +1,16 @@
+package gitpackage;
+import java.util.*;
+public class Loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i=1; i<=10; i++) {
+			System.out.println(i*2);
+		}
+
+	}
+
+}
