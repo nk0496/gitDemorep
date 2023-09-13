77aa9fc1cb58a4fade84e1b156ffcfcfd1e0fb62
Tanmay-Tambe-TKT <134093384+Tanmay-Tambe-TKT@users.noreply.github.com>
2023-09-13 14:36:10 +0530

    Added add method.....

--- src/gitpackage/GitDemo.java
+++ src/gitpackage/GitDemo.java
@@ -9,6 +9,9 @@ public class GitDemo {
 			System.out.println("This is micro learning...................................................................");
 		}
 	}
+	void add() {
+		System.out.println("Hello World");
+	}
 	
 	public static void main(String[] args) {
 	
