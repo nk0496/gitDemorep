b18b2d9361d55fc099fac449bc8f4cc304ae8a1b
Rockstar717 <warke.chetan710@gmail.com>
2023-09-13 14:39:17 +0530

    commit method second time

---
+++ src/gitpackage/Code.java
@@ -0,0 +1,14 @@
+package gitpackage;
+
+public class Code {
+
+	public void m() {
+		System.out.println("complete git process");
+	}
+	public static void main(String[] args) {
+		
+		Code c = new Code();
+		c.m();
+	}
+
+}
